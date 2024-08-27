198a2e344d00d325c2d64d5c7cba38b88eabc1d0
Muhammad Faqih <freemiumlemper300@gmail.com>
2024-08-28 00:57:14 +0700

    Add Input And Result

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const welcomeMessage = document.getElementById('pesan');
+    const welcomeMessage = document.getElementById('welcome');
     const enteredName = prompt('Halo, nama kamu siapa?:');
     if (enteredName) {
         if (welcomeMessage) {
@@ -7,3 +7,26 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 });
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const nama = document.getElementById('nama').value;
+    const tgl = document.getElementById('tgl').value;
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+    const pesan = document.getElementById('pesan').value;
+
+    if (nama && tgl && gender && pesan) {
+        const hariini = new Date().toLocaleString();
+    
+        console.debug(pesan)
+        console.debug(nama)
+        console.debug(tgl)
+        console.debug(gender)
+        const hasilText = `Kini Pukul : ${hariini}\n\nNama: ${nama}\nTanggal Lahir: ${tgl}\nJenis Kelamin: ${gender}\nPesan: ${pesan}`;
+        document.getElementById('hasil').value = hasilText;
+    } else{
+        alert('Semua kolom harus diisi!');
+        return;
+    }
+});
+
